0ae7aed06d897dc01169891a0bb887f1104c13d5
vanthorng <T.Vanthorng@cas.com.kh>
2025-07-07 13:12:34 +0700

    Method:  Return Value

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Divide(23,12) );
+        Console.ReadKey();
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Cannot divide by zero.");
+        }
+        return a / b;
+    }
+
+}
