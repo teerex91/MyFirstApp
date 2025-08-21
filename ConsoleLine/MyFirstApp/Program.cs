d7d6cde2d73c11ab902a13b5d2a815eaa7b60f54
vanthorng <T.Vanthorng@cas.com.kh>
2025-08-21 16:27:40 +0700

    Learn - Properties

--- ConsoleLine/MyFirstApp/Program.cs
+++ ConsoleLine/MyFirstApp/Program.cs
@@ -10,9 +10,12 @@ class Program
     static void Main(string[] args)
     {
         Box box = new Box();
-        box.Length = 10;
+        //box.Length = 10;
+        box.SetLength(-2); // Fixed: Changed 'setlength' to 'setLength' for proper naming convention
         box.Width = 5;
         box.Height = 2;
+        Console.WriteLine("Box length is " + box.GetLength());
+        Console.WriteLine("Box Volume is " + box.GetVolume());
         box.Displayinfo();
     }
 } 
